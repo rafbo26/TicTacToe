14751a41d3ed21ac89a39d20ea4f4be8296cef2d
Rafal Borkowski <44680497+rafbo26@users.noreply.github.com>
2022-11-04 15:40:45 +0000

    add gameOver

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ function main() {
     const poleHTMLCollection = document.getElementsByClassName('pole')
     const poleArr = [].slice.call(poleHTMLCollection);
     var turn = 'cross';
+    var game = [0,0,0,0,0,0,0,0,0];
+    var gameOver = false;
+    var combinations = [
+        [game[0], game[1], game[2]],
+        [game[3], game[4], game[5]],
+        [game[6], game[7], game[8]],
+        [game[0], game[3], game[6]],
+        [game[1], game[4], game[7]],
+        [game[2], game[5], game[8]],
+        [game[0], game[4], game[5]],
+        [game[2], game[4], game[6]]
+    ];
     
     poleArr.forEach(pole => {
             pole.addEventListener("click", addCircle); 
@@ -10,7 +22,7 @@ function main() {
     
     function addCircle(evt) {
 
-        if (evt.currentTarget.classList.contains('empty')) {
+        if (evt.currentTarget.classList.contains('empty') && !gameOver) {
 
             evt.currentTarget.classList.remove('empty');
             evt.currentTarget.classList.add('taken');
@@ -19,10 +31,26 @@ function main() {
             
             newDiv.classList.add(turn); 
             evt.currentTarget.appendChild(newDiv);
+            game[parseInt(evt.currentTarget.id[evt.currentTarget.id.length - 1]) - 1] = turn === 'cross' ? 1 : 10;
+            
+            gameOver = checkForWin();
+
             turn = turn === 'cross' ? 'circle' : 'cross';
+
         }
         
     }
+
+    function checkForWin() {
+        combinations.forEach(comb => {
+            let sum = comb.reduce((partialSum, a) => partialSum + a, 0);
+            if (sum === 3 || sum === 30) {
+                return true;
+            }
+        })
+        return false;
+    }
+
 }
 
 
