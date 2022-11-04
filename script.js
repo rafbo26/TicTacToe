adebfaef36703df34f3b428066687eabe8b1055c
Rafal Borkowski <44680497+rafbo26@users.noreply.github.com>
2022-11-04 13:11:11 +0000

    add js

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ function main() {
     const plansza = document.getElementById('plansza');
     const poleArr = document.getElementsByClassName('pole')
     
-    const newDiv = document.createElement("div");
 
-    newDiv.classList.add('circle');
-    
+
+    poleArr[0].addEventListener("click", addCircle); 
+
+    function addCircle() {
+        const newDiv = document.createElement("div");
+
+        newDiv.classList.add('circle');
+
+        poleArr[0].appendChild(newDiv);
+    }
+
 }
 
 
