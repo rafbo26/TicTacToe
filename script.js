8ed2e01b8e2c5da9570ae0b2eb075bf9c4be2ab0
Rafal Borkowski <44680497+rafbo26@users.noreply.github.com>
2022-11-04 16:00:58 +0000

    add code

--- script.js
+++ script.js
@@ -5,16 +5,6 @@ function main() {
     var turn = 'cross';
     var game = [0,0,0,0,0,0,0,0,0];
     var gameOver = false;
-    var combinations = [
-        [game[0], game[1], game[2]],
-        [game[3], game[4], game[5]],
-        [game[6], game[7], game[8]],
-        [game[0], game[3], game[6]],
-        [game[1], game[4], game[7]],
-        [game[2], game[5], game[8]],
-        [game[0], game[4], game[5]],
-        [game[2], game[4], game[6]]
-    ];
     
     poleArr.forEach(pole => {
             pole.addEventListener("click", addCircle); 
@@ -37,29 +27,34 @@ function main() {
 
             turn = turn === 'cross' ? 'circle' : 'cross';
 
+        } else if (gameOver) {
+            const msg = document.getElementById('msg');
+            msg.innerHTML = turn + ' wins';
         }
         
     }
 
     function checkForWin() {
+        let combinations = [
+            [game[0], game[1], game[2]],
+            [game[3], game[4], game[5]],
+            [game[6], game[7], game[8]],
+            [game[0], game[3], game[6]],
+            [game[1], game[4], game[7]],
+            [game[2], game[5], game[8]],
+            [game[0], game[4], game[5]],
+            [game[2], game[4], game[6]]
+        ];
+        isWin = false;
         combinations.forEach(comb => {
             let sum = comb.reduce((partialSum, a) => partialSum + a, 0);
             if (sum === 3 || sum === 30) {
-                return true;
+                isWin = true;
             }
         })
-        return false;
+        return isWin;
     }
 
 }
-
-
-
-
-
-
-
-
-
 
 main();
